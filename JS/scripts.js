33c791258974e956604e69d4d6dcf2f416732aab
cimi88 <salgueiroezequieli@gmail.com>
2025-01-02 23:19:45 -0300

    correcciones

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,19 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
     // Obtiene la ruta actual relativa al proyecto (elimina el prefijo del proyecto)
     const currentPath = window.location.pathname.replace("/proyecto-holistica", "");
 
-    links.forEach((link) => {
-        // Convierte el atributo 'href' del enlace en una ruta absoluta relativa al documento actual
-        const linkPath = new URL(link.getAttribute("href"), window.location.origin).pathname;
+    // links.forEach((link) => {
+    //     // Convierte el atributo 'href' del enlace en una ruta absoluta relativa al documento actual
+    //     const linkPath = new URL(link.getAttribute("href"), window.location.origin).pathname;
 
-        // Normaliza el linkPath eliminando el prefijo del proyecto
-        const normalizedLinkPath = linkPath.replace("/proyecto-holistica", "");
+    //     // Normaliza el linkPath eliminando el prefijo del proyecto
+    //     const normalizedLinkPath = linkPath.replace("/proyecto-holistica", "");
 
-        // Compara las rutas normalizadas
-        if (currentPath === normalizedLinkPath) {
-            link.classList.add("active"); // Agrega la clase active al enlace correspondiente
-            link.appendChild(borderDiv);
-        }
-    });
+    //     // Compara las rutas normalizadas
+    //     if (currentPath === normalizedLinkPath) {
+    //         link.classList.add("active"); // Agrega la clase active al enlace correspondiente
+    //         link.appendChild(borderDiv);
+    //     }
+    // });
 
     // Inicializacion AOS
     AOS.init({
