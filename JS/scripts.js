99643966a547a59cfdfb84c3320cf2958b6ca0b8
cimi88 <salgueiroezequieli@gmail.com>
2025-01-04 03:20:45 -0300

    modificaciones generales

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,6 +20,26 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+          e.preventDefault(); // Evita el comportamiento por defecto del enlace
+          
+          const targetId = this.getAttribute('href').substring(1); // Obtener el ID de destino
+          const targetElement = document.getElementById(targetId);
+          const navbarHeight = document.getElementById('navbar').offsetHeight; // Altura del navbar
+          
+          
+          window.scrollTo({
+              top: targetElement.offsetTop - navbarHeight, // Ajustamos el desplazamiento
+              behavior: 'smooth' // Scroll suave
+            });
+        });
+    });
+
+
+
     // Inicializacion AOS
     AOS.init({
         duration: 1200,
