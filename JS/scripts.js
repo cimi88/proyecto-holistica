d8dd54120e96d4814e9b2190003a742eccc00411
cimi88 <salgueiroezequieli@gmail.com>
2025-01-15 04:36:50 -0300

    cambios nuevos

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,27 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".nav-link");
-    const borderDiv = document.createElement("div");
-    borderDiv.classList.add("border");
-
-    // Obtiene la ruta actual relativa al proyecto (elimina el prefijo del proyecto)
-    const currentPath = window.location.pathname.replace("/proyecto-holistica", "");
-
-    links.forEach((link) => {
-        // Convierte el atributo 'href' del enlace en una ruta absoluta relativa al documento actual
-        const linkPath = new URL(link.getAttribute("href"), window.location.origin).pathname;
-
-        // Normaliza el linkPath eliminando el prefijo del proyecto
-        const normalizedLinkPath = linkPath.replace("/proyecto-holistica", "");
-
-        // Compara las rutas normalizadas
-        if (currentPath === normalizedLinkPath) {
-            link.classList.add("active"); // Agrega la clase active al enlace correspondiente
-            link.appendChild(borderDiv);
-        }
-    });
-
-
-
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
           e.preventDefault(); // Evita el comportamiento por defecto del enlace
@@ -38,10 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-
-
     // Inicializacion AOS
-    AOS.init({
-        duration: 1200,
-    });
+    AOS.init();
 });
