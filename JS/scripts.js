be3be80ae0a8f3c19c83c39fb754944db1dadf13
cimi88 <salgueiroezequieli@gmail.com>
2025-01-15 23:04:27 -0300

    cambios nuevos

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-          e.preventDefault(); // Evita el comportamiento por defecto del enlace
-          
-          const targetId = this.getAttribute('href').substring(1); // Obtener el ID de destino
-          const targetElement = document.getElementById(targetId);
-          const navbarHeight = document.getElementById('navbar').offsetHeight; // Altura del navbar
-          
-          
-          window.scrollTo({
-              top: targetElement.offsetTop - navbarHeight, // Ajustamos el desplazamiento
-              behavior: 'smooth' // Scroll suave
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault(); // Evita el comportamiento por defecto del enlace
+
+            const targetId = this.getAttribute("href").substring(1); // Obtener el ID de destino
+            const targetElement = document.getElementById(targetId);
+            const navbarHeight = document.getElementById("navbar").offsetHeight; // Altura del navbar
+
+            window.scrollTo({
+                top: targetElement.offsetTop - navbarHeight, // Ajustamos el desplazamiento
+                behavior: "smooth", // Scroll suave
             });
         });
     });
 
     // Inicializacion AOS
     AOS.init();
+
+    //EmailJS
+    const btn = document.getElementById("button");
+    document.getElementById("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        btn.value = "Enviando...";
+
+        const serviceID = "service_xqcuswe";
+        const templateID = "contact_form";
+
+        emailjs.sendForm(serviceID, templateID, this).then(
+            () => {
+                btn.value = "Enviar";
+                alert("Gracias por tu consulta ♥️\nEn breve te estaremos respondiendo!");
+            },
+            (err) => {
+                btn.value = "Enviar";
+                alert(JSON.stringify(err));
+            }
+        );
+    });
 });
