093ad6e093c1f69052e9ec0174705728eb83c26b
cimi88 <salgueiroezequieli@gmail.com>
2025-01-26 04:46:09 -0300

    nuevos cambios

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
     document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         anchor.addEventListener("click", function (e) {
             e.preventDefault(); // Evita el comportamiento por defecto del enlace
@@ -15,9 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Inicializacion AOS
-    AOS.init();
+    AOS.init({
+        once: true
+    });
 
-    //EmailJS
+    // EmailJS
     const btn = document.getElementById("button");
     document.getElementById("form").addEventListener("submit", function (event) {
         event.preventDefault();
@@ -38,4 +41,13 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         );
     });
+
+    // cambio en la animacion de AOS para pantalla con max-width: 425px
+    const cardCurso = document.querySelector(".card-curso");
+    if(window.innerWidth <= 425) {
+        cardCurso.setAttribute("data-aos", "anim-fade-down");
+    } else {
+        cardCurso.setAttribute("data-aos", "anim-fade-left");
+    }
+
 });
